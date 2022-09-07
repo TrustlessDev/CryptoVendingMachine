26f03a245e3a9b6c238843b811f8d2270882888c
Dalton <dalton@drinktoearn.io>
2022-09-07 21:24:42 +0800

    add Thermal Printer

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const Gpio = require('onoff').Gpio;
 const qrcodeScanner = new Gpio(23, 'out');
+const SerialPort = require('serialport').SerialPort;
+const Printer = require('thermalprinter');
 
-initScanner();
-startScan();
+const serialPort = new SerialPort({
+    path: '/dev/serial0',
+    baudRate: 9600
+});
+
+serialPort.on('open',function() {
+	var printer = new Printer(serialPort);
+	printer.on('ready', function() {
+		printer
+			.indent(5)
+			.horizontalLine(16)
+			.bold(true)
+			.indent(10)
+			.printLine('first line')
+			.bold(false)
+			.inverse(true)
+			.big(true)
+			.right()
+			.printLine('second line')
+			.print(function() {
+				console.log('done');
+
+                initScanner();
+                startScan();
+
+				process.exit();
+			});
+	});
+});
 
 function initScanner() {
     qrcodeScanner.writeSync(1);
